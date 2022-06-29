aecaa40aeeed74eb0f88ee9f82ef9a371e079b41
Idan Maimon <idanmaimon381891@gmail.com>
2022-06-29 13:54:58 +0300

    added a router link

--- js/router.js
+++ js/router.js
@@ -1,21 +1,21 @@
-// import noteApp from '';
 // import emailApp from '';
+import keep from './apps/keep/pages/note-index.cmp.js';
 import appHome from './pages/app-home.cmp.js'
 
 
 const routes = [
-    {
-        path: '/',
-        component: appHome
-    },
-    // {
-    //     path: '/email',
-    //     component: emailApp
-    // },
-    // {
-    //     path: '/notes',
-    //     component: noteApp
-    // },
+  {
+    path: '/',
+    component: appHome
+  },
+  // {
+  //     path: '/email',
+  //     component: emailApp
+  // },
+  {
+    path: '/notes',
+    component: keep
+  },
 ]
 
 export const router = VueRouter.createRouter({
