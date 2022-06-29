6d58d6bc930448ffb7dc09b8ce2fec0ea43041c6
Idan Maimon <idanmaimon381891@gmail.com>
2022-06-29 20:37:43 +0300

    "bug fix"

--- js/services/note.service.js
+++ js/services/note.service.js
@@ -1,5 +1,5 @@
 import { storageService } from './async-storage-service.js';
-import { utilService } from './util.service .js'
+import { utilService } from './util.service.js'
 
 
 const NOTES_KEY = 'notes';
