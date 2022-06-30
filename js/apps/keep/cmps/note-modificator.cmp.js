fe8ed8bea018e46842a21ade8d9da5a76eaaf359
Idan Maimon <idanmaimon381891@gmail.com>
2022-06-30 05:02:24 +0300

    icons fix

--- js/apps/keep/cmps/note-modificator.cmp.js
+++ js/apps/keep/cmps/note-modificator.cmp.js
@@ -1,11 +1,11 @@
 export default {
     template: `
     <section>
-            <img src="../../../../img/keep-imgs/icons/colors.svg" alt="">
-            <img src="../../../../img/keep-imgs/icons/edit.svg" alt="">
-            <img src="../../../../img/keep-imgs/icons/tag.svg" alt="">
-            <img class="dupe-note" src="../../../../img/keep-imgs/icons/copy.svg" alt="">
-            <img src="../../../../img/keep-imgs/icons/trash-can.svg" alt="">
+            <img src="img/keep-imgs/icons/colors.svg" alt="">
+            <img src="img/keep-imgs/icons/edit.svg" alt="">
+            <img src="img/keep-imgs/icons/tag.svg" alt="">
+            <img class="dupe-note" src="img/keep-imgs/icons/copy.svg" alt="">
+            <img src="img/keep-imgs/icons/trash-can.svg" alt="">
     </section>
 `,
     data() {
