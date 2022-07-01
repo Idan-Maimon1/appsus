d3d39d2f0705e3f9acbf340e1f58c73eee4754a7
Inbal Prital <inbalprital@gmail.com>
2022-07-01 21:21:39 +0300

    filter inbox mail

--- js/apps/mail/cmps/mail-filter.cmp.js
+++ js/apps/mail/cmps/mail-filter.cmp.js
@@ -1,22 +1,33 @@
+import mailFolderList from './mail-folder-list.cmp.js'
+
 export default {
   template: `
     <section class="mail-filter">
         <div class="filter-by-subject">
             <input class="search-mail" @input="filter" v-model="filterBy.subject" v-model="filterBy.body" v-model="filterBy.from" type="text" placeholder="search">
         </div>
+          <mail-folder-list @setFolderType="filter"/>
     </section>
     `,
+
+  components: {
+    mailFolderList,
+  },
+
   data() {
     return {
       filterBy: {
         subject: '',
         body: '',
         from: '',
+        to: '',
       },
     }
   },
   methods: {
-    filter() {
+    filter(type) {
+      console.log('filter mail lifter')
+      this.filterBy.to = type
       this.$emit('filtered', this.filterBy)
     },
   },
